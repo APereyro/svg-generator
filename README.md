3326be88343348d5351769f0d1fe4ef4730be407
Alan Pereyro <alanpereyro0@gmail.com>
2023-04-23 21:47:04 -0400

    fixed logo display

---
+++ README.md
@@ -0,0 +1,49 @@
+
+# Logo Generator
+
+## Badges
+
+![license](https://img.shields.io/badge/license-MIT-orange)
+
+## Description
+
+This is a command-line tool that generates a simple logo in SVG format based on user input.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [HowtoContribute](#HowtoContribute)
+- [Tests](#Tests)
+- [Questions](#Questions)
+
+## Installation
+
+To install the dependencies for this project, run: npm install
+
+## Usage
+
+To run the program, execute the following command: node index.js
+
+## Credits
+
+Thank you to all of my class mates and TA's for their help on this project.
+
+## License
+
+This project is under the MIT License.
+
+## How to Contribute
+
+Report Issues: Users can report any bugs, issues or feature requests by creating a new issue in the GitHub repository. This will help the project's maintainers to identify and fix any problems and make the application better.
+
+## Tests
+
+Clone the repository to your local machine. Navigate to the project directory using the terminal. Install the necessary dependencies
+
+## Questions
+
+If you have any questions or comments, please feel free to reach out to me at alanpereyro0@gmail.com . 
+You can also find me on https://github.com/APereyro where I am happy to collaborate on projects."
--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const { error } = require("console");
 
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+console.log(`./Assets/logo${randomNumber}.svg`)
 
-inquirer
+// const svgGenerator = () => {
+
+  inquirer
   .prompt([
     {
       type: "input",
@@ -36,40 +40,38 @@ inquirer
   ])
   .then(function (answers) {
     let svg;
-
+    
     switch (answers.shape) {
       case "circle":
         svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-                  <circle cx="150" cy="100" r="80" fill="${answers.shapeColor}" />
-                  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
-              </svg>`;
+        <circle cx="150" cy="100" r="80" fill="${answers.shapeColor}" />
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
+        </svg>`;
         break;
-
-      case "square":
-        svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-                  <rect x="90" y="40" width="120" height="120" fill="${answers.shapeColor}" />
-                  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
-              </svg>`;
-        break;
-
-      case "triangle":
-        svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-                  <polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeColor}" />
-                  <text x="150" y="150" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
-              </svg>`;
-        break;
-
-      default:
-        console.error("Invalid shape choice:", answers.shape);
-        return;
-    }
-
-    fs.writeFile("./Assets/logo.svg", svg, function (err) {
-      if (err) throw err;
-      console.log("Generated logo.svg");
-      fs.writeFile("./index.html", newHtml, function (err) {
-        if (err) throw err;
-        console.log("Updated index.html");
-      });
-    });
-  });
+        
+        case "square":
+          svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+          <rect x="90" y="40" width="120" height="120" fill="${answers.shapeColor}" />
+          <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
+          </svg>`;
+          break;
+          
+          case "triangle":
+            svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+            <polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeColor}" />
+            <text x="150" y="150" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
+            </svg>`;
+            break;
+            
+            default:
+              console.error("Invalid shape choice:", answers.shape);
+              return;
+            }
+            
+            fs.writeFile(`./Assets/logo.svg`, svg, function (err) {
+              if (err) throw err;
+              console.log("Generated logo.svg");
+            });
+          });
+        // }
+// module.exports = svgGenerator;

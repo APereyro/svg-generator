12a4e61480d97d905f925f1a8c9b31c64efb5dc6
Alan Pereyro <alanpereyro0@gmail.com>
2023-04-24 17:21:16 -0400

    added classes to the shapes and created test for all three shapes

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const { error } = require("console");
+const { Circle, Triangle, Square } = require("./lib/shapes");
 
-const randomNumber = Math.floor(Math.random() * 10) + 1;
-console.log(`./Assets/logo${randomNumber}.svg`)
-
-// const svgGenerator = () => {
-
-  inquirer
+inquirer
   .prompt([
     {
       type: "input",
@@ -40,38 +35,28 @@ console.log(`./Assets/logo${randomNumber}.svg`)
   ])
   .then(function (answers) {
     let svg;
-    
+
     switch (answers.shape) {
       case "circle":
-        svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="150" cy="100" r="80" fill="${answers.shapeColor}" />
-        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
-        </svg>`;
+        svg = new Circle();
         break;
-        
-        case "square":
-          svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-          <rect x="90" y="40" width="120" height="120" fill="${answers.shapeColor}" />
-          <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
-          </svg>`;
-          break;
-          
-          case "triangle":
-            svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-            <polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeColor}" />
-            <text x="150" y="150" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
-            </svg>`;
-            break;
-            
-            default:
-              console.error("Invalid shape choice:", answers.shape);
-              return;
-            }
-            
-            fs.writeFile(`./Assets/logo.svg`, svg, function (err) {
-              if (err) throw err;
-              console.log("Generated logo.svg");
-            });
-          });
-        // }
-// module.exports = svgGenerator;
+
+      case "square":
+        svg = new Square();
+        break;
+
+      case "triangle":
+        svg = new Triangle();
+        break;
+    }
+    svg.setColor(answers.shapeColor);
+
+    const text = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${svg.render()}<text x="150" y="150" font-size="60" text-anchor="middle" fill="${
+      answers.textColor
+    }">${answers.text}</text>`;
+
+    fs.writeFile(`./Assets/logo.svg`, text, function (err) {
+      if (err) throw err;
+      console.log("Generated logo.svg");
+    });
+  });
